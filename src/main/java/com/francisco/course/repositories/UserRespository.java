e3c52c1f3d7c27921baa79c11e9f7cfb90244e66
Francisco Viana <franciscosantos29@gmail.com>
2020-10-10 17:03:46 -0300

    Service layer, component registration

--- src/main/java/com/francisco/course/repositories/UserRespository.java
+++ src/main/java/com/francisco/course/repositories/UserRespository.java
@@ -1,9 +1,11 @@
 package com.francisco.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.francisco.course.entities.User;
 
+@Repository
 public interface UserRespository extends JpaRepository<User, Long> {
 
 }
