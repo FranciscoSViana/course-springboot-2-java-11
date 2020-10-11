2f4cc83ad86628fe99814550d556f3c2adffece3
Francisco Viana <franciscosantos29@gmail.com>
2020-10-10 22:17:03 -0300

    Category

---
+++ src/main/java/com/francisco/course/repositories/CategoryRespository.java
@@ -0,0 +1,11 @@
+package com.francisco.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.francisco.course.entities.Category;
+
+@Repository
+public interface CategoryRespository extends JpaRepository<Category, Long> {
+
+}
