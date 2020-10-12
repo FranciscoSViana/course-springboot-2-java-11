7cde8953411f1666a3ad5d989bd14390933a1c20
Francisco Viana <franciscosantos29@gmail.com>
2020-10-12 16:04:34 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/francisco/course/repositories/ProductRespository.java
+++ src/main/java/com/francisco/course/repositories/ProductRespository.java
@@ -3,7 +3,6 @@ package com.francisco.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.francisco.course.entities.Category;
 import com.francisco.course.entities.Product;
 
 @Repository
